1a1fa3c500ca86ed6a776f70974bcb067a4dbf06
ZeusFSX <zeusfsx@gmail.com>
2017-04-03 22:36:43 +0300

    good

--- Kursova/Main.cpp
+++ Kursova/Main.cpp
@@ -1,7 +1,15 @@
 #include "Header.h"
-int Cube[PMax][PMax][PMax];
+int Cube[PMax][MMax][NMax];
+
+#include <windows.h>
+#include <shlobj.h> //SHLDialog
+#include <stdio.h>
+
+#pragma comment (lib, "winmm.lib")
 
 int main() {
+	PlaySound(TEXT("mp3.wav"), NULL, SND_FILENAME);
+
 	Menu();
 	system("pause");
 	return 0;
--- Kursova/Menu.cpp
+++ Kursova/Menu.cpp
@@ -2,49 +2,49 @@
 #include "Measure.h"
 #include "SortMode.h"
 void Menu() {
-	int p, //змінна в які зберігається кількість перерізів
+	long long p, //змінна в які зберігається кількість перерізів
 		m, //змінна в якій зберігається кількість строк у кожному перерізі
 		n, //змінна в якій зберігається кількість стовбців у кожному перерізі
 		test;
 		
-	printf("Do you see if program sorted mas?\n 1 - Yes\n 2 - No\n");
+	printf("Do you see if program sorted array?\n 1 - Yes\n 2 - No\n");
 	scanf("%d", &test);
 	switch (test) {
 	case 1:
 		p = 2, m = 3, n = 4;
 		EnterData(p, m, n, 3);
-		printf("Mass before:\n");
+		printf("Array before:\n");
 		OutputData(p, m, n);
 		printf("Task 1: \n");
 		SortModeOne(p, m, n);
-		printf("Mass after:\n");
+		printf("Array after:\n");
 		OutputData(p, m, n);
 
 		EnterData(p, m, n, 3);
-		printf("Mass before:\n");
+		printf("Array before:\n");
 		OutputData(p, m, n);
 		printf("Task 2:\n");
 		SortModeTwo(p, m, n);
-		printf("Mass after:\n");
+		printf("Array after:\n");
 		OutputData(p, m, n);
 
 		EnterData(p, m, n, 3);
-		printf("Mass before:\n");
+		printf("Array before:\n");
 		OutputData(p, m, n);
 		printf("Task 3:\n");
 		SortModeThree(p, m, n);
-		printf("Mass after:\n");
+		printf("Array after:\n");
 		OutputData(p, m, n);
 
 		break;
 	case 2:
 		printf("Hello! \nPlease enter a size three-dimensional panels (p, m, n) <=500:\n");
 		scanf("%d%d%d", &p, &m, &n);
-
-		printf("\tSorted\tRandom\tReverse\n");
-		printf("Task 1: %.lf\t%.lf\t%.lf\n", ((double)MeasureModeOne(p, m, n, 1) / CLOCKS_PER_SEC) * 100, ((double)MeasureModeOne(p, m, n, 2) / CLOCKS_PER_SEC) * 100, ((double)MeasureModeOne(p, m, n, 3) / CLOCKS_PER_SEC) * 100);
-		printf("Task 2: %.lf\t%.lf\t%.lf\n", ((double)MeasureModeTwo(p, m, n, 1) / CLOCKS_PER_SEC) * 100, ((double)MeasureModeTwo(p, m, n, 2) / CLOCKS_PER_SEC) * 100, ((double)MeasureModeTwo(p, m, n, 3) / CLOCKS_PER_SEC) * 100);
-		printf("Task 3: %.lf\t%.lf\t%.lf\n", ((double)MeasureModeThree(p, m, n, 1) / CLOCKS_PER_SEC) * 100, ((double)MeasureModeThree(p, m, n, 2) / CLOCKS_PER_SEC) * 100, ((double)MeasureModeThree(p, m, n, 3) / CLOCKS_PER_SEC) * 100);
+		//printf("%d %d %d \n", p, m, n);
+		printf("\tSorted, ms\tRandom, ms\tReverse, ms\n");
+		printf("Task 1: %.lf\t\t%.lf\t\t%.lf\n", ((double)MeasureModeOne(p, m, n, 1) / CLOCKS_PER_SEC) * 100, ((double)MeasureModeOne(p, m, n, 2) / CLOCKS_PER_SEC) * 100, ((double)MeasureModeOne(p, m, n, 3) / CLOCKS_PER_SEC) * 100);
+		printf("Task 2: %.lf\t\t%.lf\t\t%.lf\n", ((double)MeasureModeTwo(p, m, n, 1) / CLOCKS_PER_SEC) * 100, ((double)MeasureModeTwo(p, m, n, 2) / CLOCKS_PER_SEC) * 100, ((double)MeasureModeTwo(p, m, n, 3) / CLOCKS_PER_SEC) * 100);
+		printf("Task 3: %.lf\t\t%.lf\t\t%.lf\n", ((double)MeasureModeThree(p, m, n, 1) / CLOCKS_PER_SEC) * 100, ((double)MeasureModeThree(p, m, n, 2) / CLOCKS_PER_SEC) * 100, ((double)MeasureModeThree(p, m, n, 3) / CLOCKS_PER_SEC) * 100);
 		break;
 	
 	default:
